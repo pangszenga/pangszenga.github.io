061e56a4f291edb1a4843fdbce662ba86166e63f
Priscilla Pang <Priscilla.Pang@keystonelaw.co.uk>
2020-04-02 14:03:33 +0100

    link changes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
 	
 	let portfolio = document.getElementsByClassName("portfolio")[0];
-	let aboutMe = document.getElementsByClassName("aboutMe")[0];
-	let showBtn = document.getElementsByClassName("button")[0];
-
-	// portfolio.style.display = "none";
-	// aboutMe.style.display = "block";
+	let title     = document.getElementsByClassName("title")[0];
+	let showBtn   = document.getElementsByClassName("button")[0];
+	let backBtn   = document.getElementsByClassName("button")[1];
+	let showcase  = document.getElementsByClassName("showcase");
 
 	function showPortfolio() {
 		portfolio.scrollIntoView()
+	}
+
+	function showAboutMe()  {
+		title.scrollIntoView();
+	}
+
+	function showHide(item, index) {
+		console.log(item)
 	}
 
 	showBtn.addEventListener("click", function () {
 		showPortfolio()
 	});
 
-	console.log("HI")
+	backBtn.addEventListener("click", function () {
+		showAboutMe()
+	});
+
+	// showcase.forEach(showHide);
 
 });//document ready
