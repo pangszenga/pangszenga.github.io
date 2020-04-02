a082396228b76b5dae9787980fb5a16fd21cafb1
Priscilla Pang <Priscilla.Pang@keystonelaw.co.uk>
2020-04-02 01:44:23 +0100

    change

--- js/app.js
+++ js/app.js
@@ -1,73 +1,20 @@
-//when mouse click, append text
-
 $(document).ready(function(){
+	
+	let portfolio = document.getElementsByClassName("portfolio")[0];
+	let aboutMe = document.getElementsByClassName("aboutMe")[0];
+	let showBtn = document.getElementsByClassName("button")[0];
 
-  const card1 = $("#card1");
-  const card2 = $("#card2");
-  const card3 = $("#card3");
-  const card4 = $("#card4");
-  const card5 = $("#card5");
-  const card6 = $("#card6");
+	// portfolio.style.display = "none";
+	// aboutMe.style.display = "block";
 
-  const portfolio = $('a[href="#portfolio"]');
-  const contact = $('a[href="#contact"]');
+	function showPortfolio() {
+		portfolio.scrollIntoView()
+	}
 
+	showBtn.addEventListener("click", function () {
+		showPortfolio()
+	});
 
-  let cover = $(".cover");
-  let cardArray = [card1, card2, card3, card4, card5, card6];
-
-  let icons = [portfolio, contact];
-
-
-
-  for(let i = 0; i < cardArray.length; i++){
-
-    cardArray[i].on("click", function(e){
-      let $selected = $(this).find(".moreDetail");
-      let $append = $selected.clone();
-      $append.removeClass("hide");
-      $append.css("display", "block");
-      console.log($selected);
-
-      cover.removeClass("hide");
-      $append.appendTo(cover);
-      e.preventDefault();// prevent page from bouncing to the top
-
-      function close() {
-        $('.cross').on('click', function(e){
-
-
-            cover.addClass('hide');
-            $append.remove();
-
-
-            });//listener ends
-        };//function ends
-
-        close();
-
-      });//listener ends
-    }// loop ends
-
-//scroll to view
-  document.querySelectorAll('a[href^="#portfolio"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-  });
-
-  document.querySelectorAll('a[href^="#contact"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-  });
+	console.log("HI")
 
 });//document ready
